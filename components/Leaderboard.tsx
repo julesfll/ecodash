d4c9fbb1323d119f727e5565515f19aeb7e49fc5
adrielk <theadrielkim@gmail.com>
2022-03-27 01:00:25 -0400

    comprehensive history slider

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -99,7 +99,13 @@ function FeaturedCard({
   );
 }
 
-export default function Leaderboard({ data }: { data: Building[] }) {
+export default function Leaderboard({
+  data,
+  graphUpperLimit,
+}: {
+  data: Building[];
+  graphUpperLimit: number;
+}) {
   return (
     <div>
       <div className="flex gap-2 mb-4">
@@ -131,7 +137,7 @@ export default function Leaderboard({ data }: { data: Building[] }) {
         })}
       </div>
 
-      <OverallGraph />
+      <OverallGraph graphUpperLimit={graphUpperLimit} />
     </div>
   );
 }
